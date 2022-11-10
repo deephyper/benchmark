b7361ac76942ff083934609322f306f9cf93b3bc
Yixuan Sun <yixuan.purdue@gmail.com>
2022-11-10 14:34:41 -0500

    added comments

--- lib/PINNBench/Burgers/benchmark.py
+++ lib/PINNBench/Burgers/benchmark.py
@@ -11,5 +11,5 @@ class PINNBurgersBenchmark(Benchmark):
     version = "0.0.1"
 
     requires = {
-        "bash-install": {"type": "cmd", "cmd": os.path.join(DIR, "../install.sh")},
+        "bash-install": {"type": "cmd", "cmd": os.path.join(DIR, "../install.sh")}, # not sure what this line should do or I should do to match this line.
     }
--- lib/PINNBench/Burgers/test.py
+++ lib/PINNBench/Burgers/test.py
@@ -5,7 +5,7 @@ DIR = os.path.dirname(os.path.abspath(__file__))
 from .benchmark import PINNBurgersBenchmark
 
 b = PINNBurgersBenchmark(
-    data_path=os.path.join(DIR, "../build/HPOBench/data/fcnet_tabular_benchmarks/")
+    data_path=os.path.join(DIR, " ") # not sure what the path should be
 )
 
 config = b.get_configuration_space(seed=1).sample_configuration()
