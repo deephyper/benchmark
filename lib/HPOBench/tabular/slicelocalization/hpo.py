9b5bf85bd2034e2277264cdb5487c39f8343a75b
Deathn0t <romainegele@gmail.com>
2022-11-21 13:24:04 +0100

    updated hpo bench benchmarks with new stopper interface for multi-fidelity

--- lib/HPOBench/tabular/slicelocalization/hpo.py
+++ lib/HPOBench/tabular/slicelocalization/hpo.py
@@ -2,7 +2,7 @@ import os
 
 DIR = os.path.dirname(os.path.abspath(__file__))
 
-from deephyper.evaluator import profile
+from deephyper.evaluator import profile, RunningJob
 from deephyper.problem import HpProblem
 
 # hpobench
@@ -16,35 +16,49 @@ problem = HpProblem(config_space=config_space)
 
 
 @profile
-def run(config, optuna_trial=None):
+def run(job: RunningJob, optuna_trial=None) -> dict:
+
     # otherwise failure
-    config.pop("job_id", None)
+    config = job.parameters
 
     from hpobench.benchmarks.nas.tabular_benchmarks import SliceLocalizationBenchmark
 
     b = SliceLocalizationBenchmark(data_path=data_path)
+    budget_space = b.get_fidelity_space().get("budget")
+    min_b, max_b = budget_space.lower, budget_space.upper
 
     if optuna_trial:
-        budget_space = b.get_fidelity_space().get("budget")
-        min_b, max_b = budget_space.lower, budget_space.upper
-        pruned = False
-        for budget_i in range(min_b, max_b):
-            eval = b.objective_function(config, fidelity=budget_i)
+
+        for budget_i in range(min_b, max_b + 1):
+            eval = b.objective_function(config, fidelity={"budget": budget_i})
             objective_i = -eval["function_value"]  # maximizing in deephyper
             optuna_trial.report(objective_i, step=budget_i)
             if optuna_trial.should_prune():
-                pruned = True
                 break
-        return {"objective": objective_i, "budget": budget_i, "pruned": pruned}
+
+        return {
+            "objective": objective_i,
+            "metadata": {"budget": budget_i, "stopped": budget_i < max_b},
+        }
+
     else:
-        eval = b.objective_function(config)
-        objective = -eval["function_value"]  # maximizing in deephyper
-        return objective
+
+        for budget_i in range(min_b, max_b + 1):
+            eval = b.objective_function(config, fidelity={"budget": budget_i})
+            objective_i = -eval["function_value"]  # maximizing in deephyper
+            job.record(budget_i, objective_i)
+            if job.stopped():
+                break
+
+        return {
+            "objective": job.observations,
+            "metadata": {"budget": budget_i, "stopped": budget_i < max_b},
+        }
 
 
 if __name__ == "__main__":
     print(problem)
     default_config = problem.default_configuration
     print(f"{default_config=}")
-    result = run(default_config)
+    result = run(RunningJob(parameters=default_config))
     print(f"{result=}")
