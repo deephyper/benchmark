9689394646c81f7870546067e3ab57eda4d92434
Tyler <tchang@anl.gov>
2023-03-20 13:05:54 -0500

    modified bench_dtlz.py to run an optimization round

--- examples/bench_dtlz.py
+++ examples/bench_dtlz.py
@@ -2,28 +2,50 @@
 import logging
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(filename)s:%(funcName)s - %(message)s",
+    format="%(asctime)s - %(levelname)s - %(filename)s:%(funcName)s - " + \
+           "%(message)s",
     force=True,
 )
 
 # Set DTLZ problem environment variables
 import os
-os.environ["DEEPHYPER_BENCHMARK_NDIMS"] = "5"
-os.environ["DEEPHYPER_BENCHMARK_NOBJS"] = "3"
-os.environ["DEEPHYPER_BENCHMARK_DTLZ_PROB"] = "2"
-os.environ["DEEPHYPER_BENCHMARK_DTLZ_OFFSET"] = "0.6"
+os.environ["DEEPHYPER_BENCHMARK_NDIMS"] = "5" # 5 vars
+os.environ["DEEPHYPER_BENCHMARK_NOBJS"] = "3" # 2 objs
+os.environ["DEEPHYPER_BENCHMARK_DTLZ_PROB"] = "2" # DTLZ2 problem
+os.environ["DEEPHYPER_BENCHMARK_DTLZ_OFFSET"] = "0.6" # [x_o, .., x_d]*=0.6
 
 # Load DTLZ benchmark suite
 import deephyper_benchmark as dhb
 # dhb.install("DTLZ")
 dhb.load("DTLZ")
 
-# Run HPO-pipeline with default configuration of hyperparameters
-from deephyper_benchmark.lib.dtlz import hpo
-from deephyper.evaluator import RunningJob
-config = hpo.problem.default_configuration
-print(config)
-res = hpo.run(RunningJob(parameters=config))
-print(f"{res=}")
 
+# Necessary IF statement otherwise it will enter in a infinite loop
+# when loading the 'run' function from a subprocess
+if __name__ == "__main__":
+    from deephyper.problem import HpProblem
+    from deephyper.search.hps import CBO
 
+    # Run HPO-pipeline with default configuration of hyperparameters
+    from deephyper_benchmark.lib.dtlz import hpo
+    from deephyper.evaluator import RunningJob, Evaluator
+    config = hpo.problem.default_configuration
+    print(config)
+    res = hpo.run(RunningJob(parameters=config))
+    print(f"{res=}")
+
+    # define the evaluator to distribute the computation
+    evaluator = Evaluator.create(
+        hpo.run,
+        method="process",
+        method_kwargs={
+            "num_workers": 2,
+        },
+    )
+
+    # define your search and execute it
+    search = CBO(hpo.problem, evaluator)
+
+    # solve with 100 evals
+    results = search.search(max_evals=100)
+    print(results)
