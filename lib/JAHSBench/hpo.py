1db221b808aee12a1d5639a5382b7580947e6078
Tyler <tchang@anl.gov>
2023-05-09 17:06:42 -0500

    updated jahs hpo defn

--- lib/JAHSBench/hpo.py
+++ lib/JAHSBench/hpo.py
@@ -1,29 +1,22 @@
 import time
 import numpy as np
-from deephyper.problem import NaProblem
+from deephyper.problem import HpProblem
 from deephyper.evaluator import profile, RunningJob
 from . import model
 
 
 # Create problem
-problem = NaProblem()
+problem = HpProblem()
 jahs_obj = model.jahs_bench()
-
-problem.hyperparameter(
-        LearningRate=(1.0e-3, 1.0), "LearningRate")
+# 2 continuous hyperparameters
+problem.add_hyperparameter((1.0e-3, 1.0), "LearningRate")
 problem.add_hyperparameter((1.0e-5, 1.0e-3), "WeightDecay")
-# 2 categorical variables
-moop_rbf.addDesign({'name': "Activation",
-                    'des_type': "categorical",
-                    'levels': ["ReLU", "Hardswish", "Mish"]})
-moop_rbf.addDesign({'name': "TrivialAugment",
-                    'des_type': "categorical",
-                    'levels': ["on", "off"]})
-# 6 integer variables
+# 2 categorical hyperparameters
+problem.add_hyperparameter(["ReLU", "Hardswish", "Mish"], "Activation")
+problem.add_hyperparameter(["on", "off"], "TrivialAugment")
+# 6 categorical architecture design variables
 for i in range(1, 7):
-    moop_rbf.addDesign({'name': f"Op{i}",
-                        'des_type': "integer",
-                        'lb': 0, 'ub': 4})
+    problem.add_hyperparameter([0, 1, 2, 3, 4], f"Op{i}")
 
 @profile
 def run(job: RunningJob, sleep=False, sleep_mean=60, sleep_noise=20) -> dict:
