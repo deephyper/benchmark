fc5578cd38f5277f0662134a35f7361aa319983b
Deathn0t <romainegele@gmail.com>
2023-07-21 15:04:43 +0000

    fix

--- lib/PINNBench/DiffusionReaction/hpo.py
+++ lib/PINNBench/DiffusionReaction/hpo.py
@@ -12,7 +12,7 @@ from pdebench.models.pinn.train import run_training
 from deephyper_benchmark.integration.torch import count_params
 from deephyper_benchmark.utils.json_utils import array_to_json
 
-from .model import FNN
+from .model import FNN, ACTIVATIONS
 
 DIR = os.path.dirname(os.path.abspath(__file__))
 DEEPHYPER_BENCHMARK_MOO = bool(int(os.environ.get("DEEPHYPER_BENCHMARK_MOO", 0)))
@@ -22,10 +22,9 @@ problem = HpProblem()
 problem.add_hyperparameter((5, 20), "num_layers", default_value=10)
 problem.add_hyperparameter((5, 100), "num_neurons", default_value=10)
 problem.add_hyperparameter((100, 1000), "epochs", default_value=100)
-problem.add_hyperparameter(
-    ["None", "elu", "relu", "selu", "sigmoid", "silu", "sin", "swish", "tanh"],
+problem.add_hyperparameter(list(ACTIVATIONS.keys()),
     "activation",
-    default_value="tanh",
+    default_value="relu",
 )
 problem.add_hyperparameter([True, False], "skip_co", default_value=False)
 problem.add_hyperparameter((0.0, 0.5), "dropout", default_value=0)
@@ -33,9 +32,9 @@ problem.add_hyperparameter((0.0, 0.5), "dropout", default_value=0)
 # Regularization hyperparameters
 problem.add_hyperparameter([True, False], "batch_norm", default_value=False)
 problem.add_hyperparameter(["None", "l2"], "regularization", default_value="None")
-problem.add_hyperparameter((0.0, 0.1), "weight_decay", default_value=0)
+problem.add_hyperparameter((1e-5, 1.0, "log-uniform"), "weight_decay", default_value=0.01)
 problem.add_hyperparameter(
-    ["Glorot normal", "Glorot uniform", "He normal", "He uniform", "zeros"],
+    ["Glorot normal", "Glorot uniform", "He normal", "He uniform"],
     "kernel_initializer",
     default_value="Glorot normal",
 )
@@ -48,7 +47,7 @@ problem.add_hyperparameter((1e-5, 1.0, "log-uniform"), "decay_gamma", default_va
 problem.add_hyperparameter(
     ["adam", "sgd", "rmsprop", "adamw"], "optimizer", default_value="adam"
 )
-problem.add_hyperparameter((1e-5, 1e-1, "log-uniform"), "lr", default_value=0.01)
+problem.add_hyperparameter((1e-5, 1e-1, "log-uniform"), "learning_rate", default_value=0.01)
 
 # Loss weights is tuned only if MOO is activated.
 if DEEPHYPER_BENCHMARK_MOO:
@@ -71,20 +70,51 @@ def run(job: RunningJob) -> dict:
     if config["decay"] == "step":
         config["decay"] = ("step", config["decay_step_size"], config["decay_gamma"])
 
+    # To avoid error: https://github.com/lululxvi/deepxde/blob/master/deepxde/optimizers/pytorch/optimizers.py#L45C4-L45C4 
+    if config["optimizer"] == "adamw":
+        config["regularization"] = "l2"
+
     stopper_callback = DeepXDEStopperCallback(job)
 
-    val_loss, test_loss, losshistory, model, duration_batch_inference = run_training(
-        net_class=FNN,
-        scenario="diff-react",
-        epochs=config["epochs"],
-        learning_rate=config["lr"],
-        model_update=500,
-        root_path=os.path.join(DIR, "../build/PDEBench-DH/pdebench/data/" + dataset),
-        flnm=dataset + ".h5",
-        config=config,
-        seed="0000",
-        callbacks=stopper_callback,
-    )
+    error_type = None
+    try:
+        val_loss, test_loss, losshistory, model, duration_batch_inference = run_training(
+            net_class=FNN,
+            scenario="diff-react",
+            epochs=config["epochs"],
+            learning_rate=config["learning_rate"],
+            model_update=500,
+            root_path=os.path.join(DIR, "../build/PDEBench-DH/pdebench/data/" + dataset),
+            flnm=dataset + ".h5",
+            config=config,
+            seed="0000",
+            callbacks=stopper_callback,
+        )
+    except torch.cuda.OutOfMemoryError:
+        error_type = "F_OOM"
+
+    # Managing failures to learn memory constraints
+    if error_type is not None:
+        metadata = {
+            "num_parameters": None,
+            "num_parameters_train": None,
+            "val_loss": None,
+            "test_rmse": None,
+            "budget": None,
+            "stopped": None,
+            "lc_train_loss": None,
+            "lc_val_loss": None,
+            "flops": None,
+            "duration_batch_inference": None,  
+        }
+
+        if DEEPHYPER_BENCHMARK_MOO:
+            objective = [error_type for _ in range(3)]
+        else:
+            objective = error_type
+
+        return {"objective": objective, "metadata": metadata}
+    
     param_count = count_params(model)
     flops = FlopCountAnalysis(model, inputs=(torch.randn(1, 3))).total()
 
