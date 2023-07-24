9b4c208cae696c192a0dfe84d5ad5412c08492b2
mathemalolly <138530010+mathemalolly@users.noreply.github.com>
2023-07-24 04:57:06 -0400

    Update model.py (#22)
    here, i made an edit of the build_and_train_function, in order to include some other hyperparameters and to use the default configuration used by the FNO team. The bestevaluate function has also been edited to give the visualizations

--- lib/FNOBench/model.py
+++ lib/FNOBench/model.py
@@ -1,13 +1,14 @@
 import sys
 from timeit import default_timer
-
+import matplotlib.pyplot as plt
 import neuralop.mpu.comm as comm
+from neuralop.models import TFNO2d
 import torch
 import wandb
 from neuralop import H1Loss, LpLoss
-from neuralop.models import TFNO
 from neuralop.training.patching import MultigridPatching2D
-from neuralop.utils import count_params
+from neuralop.utils import get_wandb_api_key, count_params
+import numpy as np
 
 from .data import load_darcy_flow_small
 
@@ -208,7 +209,7 @@ class Trainer:
             loader = validation_loader
             loader_name = ""
             errors = self.evaluate(
-                model, eval_losses, loader, output_encoder, log_prefix=loader_name
+                model, training_loss, loader, output_encoder, log_prefix=loader_name
             )
 
             for loss_name, loss_value in errors.items():
@@ -312,126 +313,234 @@ def build_and_train_model(config: dict, device="cpu", verbose: bool = 0):
     a format accepted by the TFNO benchmark and then trains the model
     """
     default_config = {
-        "n_modes_height": 16,
-        "n_modes_width": 16,
-        "hidden_channels": 32,
-        "projection_channels": 64,
-        "factorization": "tucker",
-        "rank": 0.42,
-        "learning_rate": 8e-3,
-        "batch_size": 32,
-        "epochs": 20,
+
+        # General
+        # For computing compression
+        # 'n_params_baseline': None,
+        'verbose': True,
+        # Distributed computing
+        'distributed_seed': 666,
+
+        # FNO related
         "lifting_channels": 256,
-        "n_layers": 4,
+        'data_channels': 3,
+        'n_modes_height': 16,
+        'n_modes_width': 16,
+        'hidden_channels': 32,
+        'projection_channels': 64,
+        'n_layers': 4,
+        'skip': 'linear',
+        'implementation': 'factorized',
+
         "use_mlp": False,
         "mlp": {"dropout": 0, "expansion": 0.5},
-        "weight_decay": 1e-4,
-        "n_train": 1000,
-        "scheduler_T_max": 20,
+
+        'factorization': None,
+        'rank': 1.0,
+
+        'stabilizer': None,
+        'fno_block_precision': 'half',
+
+        # Optimizer
+
+        'opt_n_epochs': 5,
+        'opt_learning_rate': 5e-3,
+        'opt_training_loss': 'h1',
+        'opt_weight_decay': 1e-4,
+
+        'opt_scheduler_T_max': 500,  # For cosine only, typically take n_epochs
+        'opt_scheduler_patience': 5,  # For ReduceLROnPlateau only
+        'opt_scheduler': 'StepLR',  # Or 'CosineAnnealingLR' OR 'ReduceLROnPlateau'
+        'opt_step_size': 60,
+        'opt_gamma': 0.5,
+
+        # Dataset related
+
+        'data_batch_size': 16,
+        'data_n_train': 1000,
+        'data_train_resolution': 16,
+        'data_n_tests': [100, 50],
+        'data_test_resolutions': [16, 32],
+        'data_test_batch_sizes': [16, 16],
+        'data_positional_encoding': True,
+
+        'data_encode_input': True,
+        'data_encode_output': False,
+
+        # Patching
+        'patching': {'levels': 0},
+        'patching_levels': 0,
+        'patching_padding': 0,
+        'patching_stitching': False,
     }
+
     default_config.update(config)
-    (
-        train_loader,
-        validation_loader,
-        test_loaders,
-        output_encoder,
-    ) = load_darcy_flow_small(
-        n_train=default_config["n_train"],
-        batch_size=default_config["batch_size"],
-        test_resolutions=[16, 32],
-        n_tests=[100, 50],
-        test_batch_sizes=[32, 32],
-    )
+    config_name = 'default'
 
-    model = TFNO(
-        n_modes=(default_config["n_modes_height"], default_config["n_modes_width"]),
-        n_layers=default_config["n_layers"],
-        lifting_channels=default_config["lifting_channels"],
-        hidden_channels=default_config["hidden_channels"],
-        projection_channels=default_config["projection_channels"],
-        factorization=default_config["factorization"],
-        rank=default_config["rank"],
-        use_mlp=default_config["use_mlp"],
-        mlp=default_config["mlp"],
-    )
+    # Print config to screen
+    if default_config['verbose']:
+        # pipe.log()
+        sys.stdout.flush()
+
+    # Loading the Darcy flow dataset
+
+    (train_loader, validation_loader, test_loaders, output_encoder) = load_darcy_flow_small(
+        n_train=default_config['data_n_train'], batch_size=default_config['data_batch_size'],
+        positional_encoding=default_config['data_positional_encoding'],
+        test_resolutions=default_config['data_test_resolutions'], n_tests=default_config['data_n_tests'],
+        test_batch_sizes=default_config['data_test_batch_sizes'],
+        encode_input=default_config['data_encode_input'], encode_output=default_config['data_encode_output'],
+        )
+
+    model = TFNO2d(data_channels=default_config["data_channels"], n_modes_height=default_config['n_modes_height'],
+                   n_modes_width=default_config["n_modes_width"],
+                   n_layers=default_config["n_layers"], lifting_channels=default_config["lifting_channels"],
+                   hidden_channels=default_config["hidden_channels"],
+                   projection_channels=default_config["projection_channels"],
+                   factorization=default_config["factorization"], rank=default_config["rank"],
+                   use_mlp=default_config["use_mlp"], mlp=default_config["mlp"], skip=default_config["skip"],
+                   implementation=default_config["implementation"],
+                   stabilizer=default_config["stabilizer"], fno_block_precision=default_config["fno_block_precision"])
     model = model.to(device)
-    n_params = count_params(model)
-    print(f"\nOur model has {n_params} parameters.")
-    sys.stdout.flush()
-    optimizer = torch.optim.Adam(
-        model.parameters(),
-        lr=default_config["learning_rate"],
-        weight_decay=default_config["weight_decay"],
-    )
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-        optimizer, T_max=default_config["scheduler_T_max"]
-    )
 
+    # Log parameter count
+    n_params = count_params(model)
+
+    if default_config['verbose']:
+        print(f'\nn_params: {n_params}')
+        sys.stdout.flush()
+
+
+    # Create the optimizer
+    optimizer = torch.optim.Adam(model.parameters(),
+                                 lr=default_config['opt_learning_rate'],
+                                 weight_decay=default_config['opt_weight_decay'])
+
+    if default_config['opt_scheduler'] == 'ReduceLROnPlateau':
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=default_config['opt_gamma'],
+                                                               patience=default_config['opt_scheduler_patience'],
+                                                               mode='min')
+    elif default_config['opt_scheduler'] == 'CosineAnnealingLR':
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=default_config['opt_scheduler_T_max'])
+    elif default_config['opt_scheduler'] == 'StepLR':
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=default_config['opt_step_size'],
+                                                    gamma=default_config['opt_gamma'])
+    else:
+        raise ValueError(f"Got { default_config['opt_scheduler']= }")
+
+    # Creating the losses
     l2loss = LpLoss(d=2, p=2)
     h1loss = H1Loss(d=2)
+    if default_config['opt_training_loss'] == 'l2':
+        train_loss = l2loss
+        eval_losses = dict(l2=train_loss)
+    elif default_config['opt_training_loss'] == 'h1':
+        train_loss = h1loss
+        eval_losses = dict(h1=train_loss)
+    else:
+        raise ValueError(f"Got training_loss={default_config['opt_training_loss']} but expected one of ['l2', 'h1']")
+    # eval_losses = {'h1': h1loss, 'l2': l2loss}
 
-    train_loss = h1loss
-    eval_losses = {"h1": h1loss, "l2": l2loss}
-    trainer = Trainer(
-        model,
-        n_epochs=default_config["epochs"],
-        device=device,
-        # mg_patching_levels=default_config["mg_patching_levels"],
-        wandb_log=False,
-        use_distributed=False,
-        verbose=True,
-    )
-    history = trainer.train(
-        train_loader,
-        validation_loader,
-        output_encoder,
-        model,
-        optimizer,
-        scheduler,
-        regularizer=False,
-        training_loss=train_loss,
-        eval_losses=eval_losses,
-    )
+    if default_config['verbose']:
+        print('\n### MODEL ###\n', model)
+        print('\n### OPTIMIZER ###\n', optimizer)
+        print('\n### SCHEDULER ###\n', scheduler)
+        print('\n### LOSSES ###')
+        print(f'\n * Train: {train_loss}')
+        print(f'\n * Test: {eval_losses}')
+        print(f'\n### Beginning Training...\n')
+        sys.stdout.flush()
+
+    trainer = Trainer(model, n_epochs=default_config['opt_n_epochs'], device=device,
+                      mg_patching_levels=default_config['patching_levels'],
+                      mg_patching_padding=default_config['patching_padding'],
+                      mg_patching_stitching=default_config['patching_stitching'], wandb_log=False, log_test_interval=1,
+                      log_output=True, use_distributed=False, verbose=default_config['verbose'])
+
+    history = trainer.train(train_loader, validation_loader, output_encoder, model, optimizer, scheduler,
+                            regularizer=False, training_loss=train_loss, eval_losses=eval_losses, )
 
     history["num_parameters"] = n_params
 
     return model, history
 
 
-def bestevaluate(best_config, device="cpu"):
-    """we design the model based on the best hyperparameter configuration,
-    we then evaluate this based on the training/validation and testing data"""
-    (
-        train_loader,
-        validation_loader,
-        test_loaders,
-        output_encoder,
-    ) = load_darcy_flow_small(
-        n_train=best_config["n_train"],
-        batch_size=best_config["batch_size"],
-        test_resolutions=[16, 32],
-        n_tests=[100, 50],
+def bestevaluate(best_config):
+    ''' we design the model based on the best hyperparameter configuration,
+     we then evaluate this based on the training/validation and testing data '''
+
+    (train_loader, validation_loader, test_loaders, output_encoder) = load_darcy_flow_small(
+        n_train=best_config['data_n_train'], batch_size=best_config['data_batch_size'],
+        positional_encoding=best_config['data_positional_encoding'],
+        test_resolutions=[16, 32], n_tests=[100, 50],
         test_batch_sizes=[32, 32],
-    )
+        encode_input=best_config['data_encode_input'], encode_output=best_config['data_encode_output'],
+        )
     # the build_and_train_model function
     # gives the training and validation losses
     best_model, best_history = build_and_train_model(best_config, verbose=1)
     # next we generate the testing losses
-    trainer = Trainer(
-        best_model,
-        n_epochs=best_config["epochs"],
-        device=device,
-        wandb_log=False,
-        use_distributed=False,
-        verbose=True,
-    )
+    trainer = Trainer(best_model, n_epochs=best_config['opt_n_epochs'],
+                      device=device,
+                      mg_patching_levels=best_config['patching_levels'],
+                      mg_patching_padding=best_config['patching_padding'],
+                      mg_patching_stitching=best_config['patching_stitching'],
+                      wandb_log=best_config['wandb_log'],
+                      log_test_interval=best_config['wandb_log_test_interval'],
+                      log_output=best_config['wandb_log_output'],
+                      use_distributed=best_config['distributed_use_distributed'],
+                      verbose=best_config['verbose'])
     for loader_name, loader in test_loaders.items():
         l2loss = LpLoss(d=2, p=2)
         h1loss = H1Loss(d=2)
-        eval_losses = {"h1": h1loss, "l2": l2loss}
-        errors = trainer.evaluate(
-            best_model, eval_losses, loader, output_encoder, log_prefix=loader_name
-        )
+        eval_losses = {'h1': h1loss, 'l2': l2loss}
+        errors = trainer.evaluate(best_model, eval_losses, loader, output_encoder, log_prefix=loader_name)
         for loss_name, loss_value in errors.items():
             best_history[loss_name] = loss_value
+
+    test_samples = test_loaders[32].dataset
+
+    fig = plt.figure(figsize=(7, 7))
+    for index in range(3):
+        data = test_samples[index]
+        # Input x
+        x = data['x']
+        # Ground-truth
+        y = data['y']
+        # Model prediction
+        out = best_model(x.unsqueeze(0))
+
+        ax = fig.add_subplot(4, 4, index * 4 + 1)
+        ax.imshow(x[0], cmap='gray')
+        if index == 0:
+            ax.set_title('Input x')
+        plt.xticks([], [])
+        plt.yticks([], [])
+
+        ax = fig.add_subplot(4, 4, index * 4 + 2)
+        ax.imshow(y.squeeze())
+        if index == 0:
+            ax.set_title('Ground-truth y')
+        plt.xticks([], [])
+        plt.yticks([], [])
+
+        ax = fig.add_subplot(4, 4, index * 4 + 3)
+        ax.imshow(out.squeeze().detach().numpy())
+        if index == 0:
+            ax.set_title('Model prediction')
+        plt.xticks([], [])
+        plt.yticks([], [])
+
+        ax = fig.add_subplot(4, 4, index * 4 + 4)
+        err = (np.abs(y.squeeze() - out.detach().numpy())) ** 2
+        im = ax.imshow(err.squeeze(), cmap="plasma", aspect="auto")
+        plt.colorbar(im, label="$|y - \hat{y}|^2$")
+        if index == 0:
+            ax.set_title('Squared Error')
+        plt.xticks([], [])
+        plt.yticks([], [])
+
+    fig.suptitle('Inputs, ground-truth output, prediction and absolute error.', y=0.98)
+    plt.tight_layout()
+    fig.show()
     return best_history
