4fc10c7011c1c6de80b3c73788af136923d8a620
Deathn0t <romainegele@gmail.com>
2024-01-15 10:52:56 +0100

    updated LCDB/hpo

--- lib/LCDB/hyperparameter_optimization/hpo.py
+++ lib/LCDB/hyperparameter_optimization/hpo.py
@@ -9,7 +9,12 @@ DIR = os.path.dirname(os.path.abspath(__file__))
 from deephyper.evaluator import RunningJob, profile
 from deephyper.problem import HpProblem
 from lcdb.analysis import read_csv_results
-from lcdb.analysis.json import QueryAnchorValues, QueryMetricValuesFromAnchors
+from lcdb.analysis.json import (
+    QueryAnchorValues,
+    QueryMetricValuesFromAnchors,
+    QueryEpochValues,
+    QueryMetricValuesFromEpochs,
+)
 from lcdb.analysis.score import balanced_accuracy_from_confusion_matrix
 from lcdb.utils import import_attr_from_module
 
@@ -20,10 +25,19 @@ from lcdb.utils import import_attr_from_module
 #     os.environ.get("DEEPHYPER_BENCHMARK_PROP_REAL_RUN_TIME", 1.0)
 # )
 
-DEEPHYPER_BENCHMARK_WORKFLOW = "lcdb.workflow.sklearn.KNNWorkflow"
+# DEEPHYPER_BENCHMARK_FIDELITY = os.environ.get("DEEPHYPER_BENCHMARK_FIDELITY", "anchor")
+# DEEPHYPER_BENCHMARK_WORKFLOW = "lcdb.workflow.sklearn.KNNWorkflow"
+# DEEPHYPER_BENCHMARK_CSV = os.environ.get(
+#     "DEEPHYPER_BENCHMARK_CSV",
+#     "/Users/romainegele/Documents/Research/LCDB/lcdb/publications/2023-neurips/experiments/alcf/polaris/knn/output/lcdb.workflow.sklearn.KNNWorkflow/3/42-42-42/results.csv.gz",
+# )
+DEEPHYPER_BENCHMARK_MAX_FIDELITY = int(
+    os.environ.get("DEEPHYPER_BENCHMARK_MAX_FIDELITY", 100)
+)
+DEEPHYPER_BENCHMARK_FIDELITY = os.environ.get("DEEPHYPER_BENCHMARK_FIDELITY", "epoch")
 DEEPHYPER_BENCHMARK_CSV = os.environ.get(
     "DEEPHYPER_BENCHMARK_CSV",
-    "/Users/romainegele/Documents/Research/LCDB/lcdb/publications/2023-neurips/experiments/alcf/polaris/knn/output/lcdb.workflow.sklearn.KNNWorkflow/3/42-42-42/results.csv.gz",
+    "/Users/romainegele/Documents/Research/LCDB/lcdb/publications/2023-neurips/experiments/alcf/polaris/densenn/output/lcdb.workflow.keras.DenseNNWorkflow/3/42-42-42/results.csv.gz",
 )
 
 
@@ -36,32 +50,70 @@ with gzip.GzipFile(DEEPHYPER_BENCHMARK_CSV, "rb") as f:
     r_df, r_df_failed = read_csv_results(f)
 DEEPHYPER_BENCHMARK_DATA = r_df
 
+if DEEPHYPER_BENCHMARK_FIDELITY == "anchor":
+    query_anchor_values = QueryAnchorValues()
+    query_confusion_matrix_values_on_val = QueryMetricValuesFromAnchors(
+        "confusion_matrix", split_name="val"
+    )
+    query_confusion_matrix_values_on_test = QueryMetricValuesFromAnchors(
+        "confusion_matrix", split_name="test"
+    )
+elif DEEPHYPER_BENCHMARK_FIDELITY == "epoch":
+    # Load the learning "epoch"-curve at last epoch
+    query_anchor_values_ = QueryEpochValues()
+    query_confusion_matrix_values_on_val_ = QueryMetricValuesFromEpochs(
+        "confusion_matrix", split_name="val"
+    )
+    query_confusion_matrix_values_on_test_ = QueryMetricValuesFromEpochs(
+        "confusion_matrix", split_name="test"
+    )
+    query_anchor_values = lambda x: query_anchor_values_(x)[-1]
+    query_confusion_matrix_values_on_val = (
+        lambda x: query_confusion_matrix_values_on_val_(x)[-1]
+    )
+    query_confusion_matrix_values_on_test = (
+        lambda x: query_confusion_matrix_values_on_test_(x)[-1]
+    )
+
+    source = DEEPHYPER_BENCHMARK_DATA["m:json"]
+    values = source.apply(lambda x: query_confusion_matrix_values_on_val(x)).to_list()
+    mask = [len(x) > 0 for x in values]
+    DEEPHYPER_BENCHMARK_DATA = DEEPHYPER_BENCHMARK_DATA[mask]
+
+    source = DEEPHYPER_BENCHMARK_DATA["m:json"]
+    values = source.apply(lambda x: query_confusion_matrix_values_on_test(x)).to_list()
+    mask = [len(x) > 0 for x in values]
+    DEEPHYPER_BENCHMARK_DATA = DEEPHYPER_BENCHMARK_DATA[mask]
+else:
+    raise ValueError(f"Unknown fidelity: {DEEPHYPER_BENCHMARK_FIDELITY}")
+
 problem.add_hyperparameter((0, len(DEEPHYPER_BENCHMARK_DATA) - 1), "eval_id")
-query_anchor_values = QueryAnchorValues()
-query_confusion_matrix_values_on_val = QueryMetricValuesFromAnchors(
-    "confusion_matrix", split_name="val"
-)
 
 
-def query_balanced_error_rate_values_on_val(x):
+def query_coefficient_of_determiniation_values_on_val(x):
+    cm = query_confusion_matrix_values_on_val(x)
+    num_classes = len(cm[0])
+    balanced_error_rate_baseline = 1 - 1 / num_classes
     return list(
         map(
-            lambda x: 1 - balanced_accuracy_from_confusion_matrix(x),
-            query_confusion_matrix_values_on_val(x),
+            lambda x: 1
+            - (1 - balanced_accuracy_from_confusion_matrix(x))
+            / balanced_error_rate_baseline,
+            cm,
         )
     )
 
 
-query_confusion_matrix_values_on_test = QueryMetricValuesFromAnchors(
-    "confusion_matrix", split_name="test"
-)
-
-
-def query_balanced_error_rate_values_on_test(x):
+def query_coefficient_of_determiniation_values_on_test(x):
+    cm = query_confusion_matrix_values_on_test(x)
+    num_classes = len(cm[0])
+    balanced_error_rate_baseline = 1 - 1 / num_classes
     return list(
         map(
-            lambda x: 1 - balanced_accuracy_from_confusion_matrix(x),
-            query_confusion_matrix_values_on_test(x),
+            lambda x: 1
+            - (1 - balanced_accuracy_from_confusion_matrix(x))
+            / balanced_error_rate_baseline,
+            cm,
         )
     )
 
@@ -73,10 +125,26 @@ def run(job: RunningJob, optuna_trial=None) -> dict:
 
     source = DEEPHYPER_BENCHMARK_DATA.iloc[eval_id]["m:json"]
     anchor_values = query_anchor_values(source)
-    objective_val_values = query_balanced_error_rate_values_on_val(source)
-    objective_test_values = query_balanced_error_rate_values_on_test(source)
+    objective_val_values = query_coefficient_of_determiniation_values_on_val(source)
+    objective_test_values = query_coefficient_of_determiniation_values_on_test(source)
+    length = min(len(objective_val_values), len(objective_test_values))
+    anchor_values = anchor_values[:length]
+    objective_val_values = objective_val_values[:length]
+    objective_test_values = objective_test_values[:length]
 
-    anchor_values = anchor_values[: len(objective_val_values)]
+    objective_val_values = np.array(objective_val_values)
+    objective_test_values = np.array(objective_test_values)
+    anchor_values = np.array(anchor_values)
+
+    mask = anchor_values <= DEEPHYPER_BENCHMARK_MAX_FIDELITY
+    anchor_values = anchor_values[mask].tolist()
+    objective_val_values = objective_val_values[mask].tolist()
+    objective_test_values = objective_test_values[mask].tolist()
+
+    if anchor_values[-1] <= DEEPHYPER_BENCHMARK_MAX_FIDELITY:
+        anchor_values.append(DEEPHYPER_BENCHMARK_MAX_FIDELITY)
+        objective_val_values.append(objective_val_values[-1])
+        objective_test_values.append(objective_test_values[-1])
 
     min_b, max_b = anchor_values[0], anchor_values[-1]
 
@@ -86,7 +154,7 @@ def run(job: RunningJob, optuna_trial=None) -> dict:
 
     if optuna_trial:
         for i, budget_i in enumerate(anchor_values):
-            objective_i = -objective_val_values[i]
+            objective_i = objective_val_values[i]
 
             optuna_trial.report(objective_i, step=budget_i)
             if optuna_trial.should_prune():
@@ -96,9 +164,9 @@ def run(job: RunningJob, optuna_trial=None) -> dict:
 
     else:
         for i, budget_i in enumerate(anchor_values):
-            objective_i = -objective_val_values[i]
+            objective_i = objective_val_values[i]
 
-            job.record(budget_i, 1+objective_i)
+            job.record(budget_i, objective_i)
             if job.stopped():
                 break
 
@@ -110,8 +178,8 @@ def run(job: RunningJob, optuna_trial=None) -> dict:
     metadata = {
         "budget": budget_i,
         "stopped": budget_i < max_b,
-        "objective_test": -objective_test_values[-1],
-        "objective_val": -objective_val_values[-1],
+        "objective_test": objective_test_values[-1],
+        "objective_val": objective_val_values[-1],
     }
     metadata.update(other_metadata)
 
@@ -123,7 +191,10 @@ def run(job: RunningJob, optuna_trial=None) -> dict:
 
 if __name__ == "__main__":
     print(problem)
-    default_config = problem.default_configuration
-    print(f"{default_config=}")
-    result = run(RunningJob(parameters=default_config))
-    print(f"{result=}")
+    # default_config = problem.default_configuration
+    for eval_id in range(10):
+        default_config = dict(eval_id=eval_id)
+        print(f"{default_config=}")
+        result = run(RunningJob(parameters=default_config))
+        print(f"{result=}")
+        print()
