cd02f0ddb6f1bcf0cb29c9b716293908e7d98e64
Deathn0t <romainegele@gmail.com>
2025-03-14 17:38:42 +0100

    upload all other benchmarks for c_bbo

--- src/deephyper_benchmark/lib/c_bbo/rosen/hpo_benchmark.py
+++ src/deephyper_benchmark/lib/c_bbo/rosen/hpo_benchmark.py
@@ -1,39 +1,16 @@
 """Module defining the problem and run-function of the benchmark."""
 
+import os
 import time
 
 import numpy as np
-
+from deephyper.evaluator import RunningJob, profile
 from deephyper.hpo import HpProblem
-from deephyper.evaluator import profile, RunningJob
+from scipy.optimize import rosen
+
 from deephyper_benchmark import HPOBenchmark, HPOScorer
 
 __all__ = ["benchmark"]
-
-
-def hartmann6D(x):
-    alpha = np.array([1.0, 1.2, 3.0, 3.2])
-    A = np.array(
-        [
-            [10, 3, 17, 3.5, 1.7, 8],
-            [0.05, 10, 17, 0.1, 8, 14],
-            [3, 3.5, 1.7, 10, 17, 8],
-            [17, 8, 0.05, 10, 0.1, 14],
-        ]
-    )
-    P = 1e-4 * np.array(
-        [
-            [1312, 1696, 5569, 124, 8283, 5886],
-            [2329, 4135, 8307, 3736, 1004, 9991],
-            [2348, 1451, 3522, 2883, 3047, 6650],
-            [4047, 8828, 8732, 5743, 1091, 381],
-        ]
-    )
-    X = np.array([x for _ in range(4)])
-    inner = np.sum(np.multiply(A, np.square(X - P)), axis=1)
-    outer = np.sum(alpha * np.exp(-inner))
-    y = -(2.58 + outer) / 1.94
-    return y
 
 
 @profile
@@ -48,25 +25,16 @@ def run_function(job: RunningJob, sleep=False, sleep_mean=60, sleep_noise=20) ->
     x = np.array([config[k] for k in config if "x" in k])
     x = np.asarray_chkfinite(x)  # ValueError if any NaN or Inf
 
-    return -hartmann6D(x)
+    return -rosen(x)
 
 
-class Hartmann6DHPOScorer(HPOScorer):
-    """A class defining performance evaluators for the Hartmann6D problem."""
+class RosenHPOScorer(HPOScorer):
+    """A class defining performance evaluators for the Rosen problem."""
 
-    def __init__(self):
-        self.p_num = 6
-        self.x_max = np.array(
-            [
-                0.20169,
-                0.150011,
-                0.476874,
-                0.275332,
-                0.311652,
-                0.6573,
-            ]
-        )
-        self.y_max = 3.32237
+    def __init__(self, p_num):
+        self.p_num = p_num
+        self.x_max = np.ones(self.p_num)
+        self.y_max = 0.0
 
     def simple_regret(self, y: np.ndarray) -> np.ndarray:
         """Compute the regret of a list of given solution.
@@ -91,12 +59,15 @@ class Hartmann6DHPOScorer(HPOScorer):
         return np.cumsum(self.simple_regret(y))
 
 
-class Hartmann6DHPOBenchmark(HPOBenchmark):
+class RosenHPOBenchmark(HPOBenchmark):
+    def refresh_settings(self):
+        self.DEEPHYPER_BENCHMARK_NDIMS = int(os.environ.get("DEEPHYPER_BENCHMARK_NDIMS", 5))
+
     @property
     def problem(self):
-        domain = (0.0, 1.0)
+        domain = (-5.0, 10.0)
         problem = HpProblem()
-        for i in range(6):
+        for i in range(self.DEEPHYPER_BENCHMARK_NDIMS):
             problem.add_hyperparameter(domain, f"x{i}")
         return problem
 
@@ -106,7 +77,7 @@ class Hartmann6DHPOBenchmark(HPOBenchmark):
 
     @property
     def scorer(self):
-        return Hartmann6DHPOScorer()
+        return RosenHPOScorer(self.DEEPHYPER_BENCHMARK_NDIMS)
 
 
-benchmark = Hartmann6DHPOBenchmark()
+benchmark = RosenHPOBenchmark()
--- src/deephyper_benchmark/lib/c_bbo/shekel/hpo_benchmark.py
+++ src/deephyper_benchmark/lib/c_bbo/shekel/hpo_benchmark.py
@@ -1,5 +1,6 @@
 """Module defining the problem and run-function of the benchmark."""
 
+import os
 import time
 
 import numpy as np
@@ -11,29 +12,18 @@ from deephyper_benchmark import HPOBenchmark, HPOScorer
 __all__ = ["benchmark"]
 
 
-def hartmann6D(x):
-    alpha = np.array([1.0, 1.2, 3.0, 3.2])
-    A = np.array(
+def shekel(x):
+    m = 10
+    beta = 0.1 * np.array([1, 2, 2, 4, 4, 6, 3, 7, 5, 5]).T
+    C = np.array(
         [
-            [10, 3, 17, 3.5, 1.7, 8],
-            [0.05, 10, 17, 0.1, 8, 14],
-            [3, 3.5, 1.7, 10, 17, 8],
-            [17, 8, 0.05, 10, 0.1, 14],
+            [4, 1, 8, 6, 3, 2, 5, 8, 6, 7],
+            [4, 1, 8, 6, 7, 9, 3, 1, 2, 3.6],
+            [4, 1, 8, 6, 3, 2, 5, 8, 6, 7],
+            [4, 1, 8, 6, 7, 9, 3, 1, 2, 3.6],
         ]
     )
-    P = 1e-4 * np.array(
-        [
-            [1312, 1696, 5569, 124, 8283, 5886],
-            [2329, 4135, 8307, 3736, 1004, 9991],
-            [2348, 1451, 3522, 2883, 3047, 6650],
-            [4047, 8828, 8732, 5743, 1091, 381],
-        ]
-    )
-    X = np.array([x for _ in range(4)])
-    inner = np.sum(np.multiply(A, np.square(X - P)), axis=1)
-    outer = np.sum(alpha * np.exp(-inner))
-    y = -(2.58 + outer) / 1.94
-    return y
+    return -sum([1 / (np.sum((x - C[:, i]) ** 2) + beta[i]) for i in range(m)])
 
 
 @profile
@@ -48,25 +38,16 @@ def run_function(job: RunningJob, sleep=False, sleep_mean=60, sleep_noise=20) ->
     x = np.array([config[k] for k in config if "x" in k])
     x = np.asarray_chkfinite(x)  # ValueError if any NaN or Inf
 
-    return -hartmann6D(x)
+    return -shekel(x)
 
 
-class Hartmann6DHPOScorer(HPOScorer):
-    """A class defining performance evaluators for the Hartmann6D problem."""
+class ShekelHPOScorer(HPOScorer):
+    """A class defining performance evaluators for the Shekel problem."""
 
     def __init__(self):
-        self.p_num = 6
-        self.x_max = np.array(
-            [
-                0.20169,
-                0.150011,
-                0.476874,
-                0.275332,
-                0.311652,
-                0.6573,
-            ]
-        )
-        self.y_max = 3.32237
+        self.p_num = 10
+        self.x_max = np.full(self.p_num, fill_value=4.0)
+        self.y_max = 10.5364
 
     def simple_regret(self, y: np.ndarray) -> np.ndarray:
         """Compute the regret of a list of given solution.
@@ -91,12 +72,12 @@ class Hartmann6DHPOScorer(HPOScorer):
         return np.cumsum(self.simple_regret(y))
 
 
-class Hartmann6DHPOBenchmark(HPOBenchmark):
+class ShekelHPOBenchmark(HPOBenchmark):
     @property
     def problem(self):
-        domain = (0.0, 1.0)
+        domain = (0.0, 10.0)
         problem = HpProblem()
-        for i in range(6):
+        for i in range(10):
             problem.add_hyperparameter(domain, f"x{i}")
         return problem
 
@@ -106,7 +87,7 @@ class Hartmann6DHPOBenchmark(HPOBenchmark):
 
     @property
     def scorer(self):
-        return Hartmann6DHPOScorer()
+        return ShekelHPOScorer()
 
 
-benchmark = Hartmann6DHPOBenchmark()
+benchmark = ShekelHPOBenchmark()
