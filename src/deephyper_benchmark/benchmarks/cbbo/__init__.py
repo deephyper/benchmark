eaaad23529b0c0b44472ff9534284096477b4675
Deathn0t <romainegele@gmail.com>
2025-03-24 17:17:44 +0100

    adding benchmarks to top-level __init__

--- src/deephyper_benchmark/benchmarks/cbbo/__init__.py
+++ src/deephyper_benchmark/benchmarks/cbbo/__init__.py
@@ -2,5 +2,14 @@
 
 from .ackley import AckleyBenchmark
 from .branin import BraninBenchmark
+from .easom import EasomBenchmark
+from .griewank import GriewankBenchmark
+from .hartmann6D import Hartmann6DBenchmark
 
-__all__ = ["AckleyBenchmark", "BraninBenchmark"]
+__all__ = [
+    "AckleyBenchmark",
+    "BraninBenchmark",
+    "EasomBenchmark",
+    "GriewankBenchmark",
+    "Hartmann6DBenchmark",
+]
