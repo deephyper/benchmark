84553b7a3b8b153982a3f4665149a4d4d3575928
Deathn0t <romainegele@gmail.com>
2025-03-24 16:57:20 +0100

    updating branin

--- src/deephyper_benchmark/benchmarks/cbbo/branin.py
+++ src/deephyper_benchmark/benchmarks/cbbo/branin.py
@@ -1,12 +1,13 @@
 """here."""
 
-import numpy as np
-import time
+import functools
 
-from deephyper.evaluator import RunningJob
-from deephyper.evaluator import profile
+import numpy as np
 from deephyper.hpo import HpProblem
+
 from deephyper_benchmark import HPOBenchmark, HPOScorer
+
+from .utils import run_function
 
 
 def branin(x):
@@ -22,22 +23,7 @@ def branin(x):
     s = 10.0
     t = 1.0 / (8.0 * np.pi)
     y = a * (x[1] - b * x[0] ** 2 + c * x[0] - r) ** 2 + s * (1 - t) * np.cos(x[0]) + s
-    return y
-
-
-@profile
-def run_function(job: RunningJob, sleep=False, sleep_mean=60, sleep_noise=20) -> dict:  # noqa: D103
-    config = job.parameters
-
-    if sleep:
-        t_sleep = np.random.normal(loc=sleep_mean, scale=sleep_noise)
-        t_sleep = max(t_sleep, 0)
-        time.sleep(t_sleep)
-
-    x = np.array([config[k] for k in config if "x" in k])
-    x = np.asarray_chkfinite(x)  # ValueError if any NaN or Inf
-
-    return -branin(x)
+    return -y
 
 
 class BraninHPOScorer(HPOScorer):
@@ -47,28 +33,6 @@ class BraninHPOScorer(HPOScorer):
         self.nparams = 2
         self.x_max = np.array([[-np.pi, 12.275], [np.pi, 2.275], [9.42478, 2.475]])
         self.y_max = -0.397887
-
-    def simple_regret(self, y: np.ndarray) -> np.ndarray:
-        """Compute the regret of a list of given solution.
-
-        Args:
-            y (np.ndarray): An array of solutions.
-
-        Returns:
-            np.ndarray: An array of regret values.
-        """
-        return self.y_max - y
-
-    def cumul_regret(self, y: np.ndarray) -> np.ndarray:
-        """Compute the cumulative regret of an array of ordered given solution.
-
-        Args:
-            y (np.ndarray): An array of solutions.
-
-        Returns:
-            np.ndarray: An array of cumulative regret values.
-        """
-        return np.cumsum(self.simple_regret(y))
 
 
 class BraninBenchmark(HPOBenchmark):
@@ -83,7 +47,7 @@ class BraninBenchmark(HPOBenchmark):
 
     @property
     def run_function(self):  # noqa: D102
-        return run_function
+        return functools.partial(run_function, bb_func=branin)
 
     @property
     def scorer(self):  # noqa: D102
