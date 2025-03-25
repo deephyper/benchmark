b084b004da604f28eb295c471109d9eeafd3ddcd
Deathn0t <romainegele@gmail.com>
2025-03-25 08:53:39 +0100

    adding tests for cbbo

--- tests/test_cbbo.py
+++ tests/test_cbbo.py
@@ -1,6 +1,7 @@
-"""here."""
+"""Test module for CBBO benchmark functions."""
 
 from deephyper.hpo import RandomSearch
+
 import deephyper_benchmark.benchmarks.cbbo as bench
 
 
@@ -13,6 +14,11 @@ def test_cbbo_benchmarks(tmp_path="."):
         bench.EasomBenchmark(),
         bench.GriewankBenchmark(),
         bench.Hartmann6DBenchmark(),
+        bench.LevyBenchmark(),
+        bench.MichalBenchmark(),
+        bench.RosenBenchmark(),
+        bench.SchwefelBenchmark(),
+        bench.ShekelBenchmark(),
     ]
 
     max_evals = 25
@@ -20,5 +26,8 @@ def test_cbbo_benchmarks(tmp_path="."):
         search = RandomSearch(b.problem, b.run_function, log_dir=tmp_path)
         results = search.search(max_evals)
         assert len(results) == max_evals
+        assert hasattr(b.scorer, "nparams")
+        assert hasattr(b.scorer, "x_max")
+        assert hasattr(b.scorer, "y_max")
         cumul_regret = b.scorer.cumul_regret(results.objective)
         assert len(cumul_regret) == max_evals
